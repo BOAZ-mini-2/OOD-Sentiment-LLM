218854ca7e9984c05dad186dd22ae400157eaa6a
likeabies <101162337+likeabies@users.noreply.github.com>
2025-11-27 22:29:20 +0900

    Add mu_md and inv_cov attributes to class

--- check_perform/final_result.py
+++ check_perform/final_result.py
@@ -26,6 +26,8 @@ class OODEvaluator:
         self.X_val = np.asarray(X_val)
         self.y_val = np.asarray(y_val)
         self.probs_val = np.asarray(probs_val)
+        self.mu_md = np.asarray(mu_md)
+        self.inv_cov = np.asarray(inv_cov)
 
         # IND / OOD 분리
         self.X_IND = self.X_val[self.y_val == 0]
